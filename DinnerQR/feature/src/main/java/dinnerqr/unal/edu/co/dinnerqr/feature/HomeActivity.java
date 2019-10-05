0b535d955297ba62ef6e725363f156b441878901
jdpp95 <jdporrasp@unal.edu.co>
2019-10-04 22:04:57 -0500

    DinnerQR initial commit

---
+++ DinnerQR/feature/src/main/java/dinnerqr/unal/edu/co/dinnerqr/feature/HomeActivity.java
@@ -0,0 +1,13 @@
+package dinnerqr.unal.edu.co.dinnerqr.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
